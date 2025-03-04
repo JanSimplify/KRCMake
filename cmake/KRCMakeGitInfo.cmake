90620eca922ab9675f57a51d094f405c0d6eb7f4
JanSimplify <101509908+JanSimplify@users.noreply.github.com>
2025-03-05 01:47:52 +0800

    feat: 添加krcmake_get_git_hash

--- cmake/KRCMakeGitInfo.cmake
+++ cmake/KRCMakeGitInfo.cmake
@@ -112,3 +112,64 @@ function(krcmake_get_git_describe)
 
     set("${arg_OUTPUT_VARIABLE}" "${git_describe}" PARENT_SCOPE)
 endfunction()
+
+# krcmake_get_git_hash(
+#   OUTPUT_VARIABLE <outvar>
+#   WORKING_DIRECTORY <dir>
+#   [QUIET]
+# )
+function(krcmake_get_git_hash)
+    set(options "QUIET")
+    set(one_value_keywords "OUTPUT_VARIABLE;WORKING_DIRECTORY")
+    set(multi_value_keywords)
+
+    cmake_parse_arguments(
+        PARSE_ARGV 0
+        "arg"
+        "${options}"
+        "${one_value_keywords}"
+        "${multi_value_keywords}"
+    )
+
+    if(NOT arg_OUTPUT_VARIABLE)
+        message(FATAL_ERROR "Missing required value: OUTPUT_VARIABLE")
+    endif()
+
+    if(arg_KEYWORDS_MISSING_VALUES)
+        message(FATAL_ERROR "Missing required value: ${arg_KEYWORDS_MISSING_VALUES}")
+    endif()
+
+    if(arg_UNPARSED_ARGUMENTS)
+        message(FATAL_ERROR "Unparsed arguments: ${arg_UNPARSED_ARGUMENTS}")
+    endif()
+
+    if(NOT Git_FOUND)
+        if(NOT arg_QUIET)
+            message(
+                FATAL_ERROR
+                "Could not find \"Git\" package configuration file."
+            )
+        endif()
+        return()
+    endif()
+
+    set(cmd_args
+        COMMAND "${GIT_EXECUTABLE}" rev-parse HEAD
+        WORKING_DIRECTORY "${arg_WORKING_DIRECTORY}"
+        RESULT_VARIABLE result
+        OUTPUT_VARIABLE git_hash
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+
+    if(arg_QUIET)
+        list(APPEND cmd_args "ERROR_QUIET")
+    endif()
+
+    execute_process(${cmd_args})
+
+    if(result AND NOT arg_QUIET)
+        message(FATAL_ERROR "Failed to get git hash: ${result}")
+    endif()
+
+    set("${arg_OUTPUT_VARIABLE}" "${git_hash}" PARENT_SCOPE)
+endfunction()
