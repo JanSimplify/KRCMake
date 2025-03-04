7347a50c5dfe4e7fc93d61c7e97b50ba524bf909
JanSimplify <101509908+JanSimplify@users.noreply.github.com>
2025-03-05 01:38:35 +0800

    feat: 添加krcmake_get_git_describe

--- cmake/KRCMakeGitInfo.cmake
+++ cmake/KRCMakeGitInfo.cmake
@@ -27,3 +27,88 @@ SOFTWARE.
 cmake_minimum_required(VERSION 3.19 FATAL_ERROR)
 
 include_guard(GLOBAL)
+
+find_package(Git QUIET)
+
+# krcmake_get_git_describe(
+#   OUTPUT_VARIABLE <outvar>
+#   WORKING_DIRECTORY <dir>
+#   [QUIET]
+#   [ALL]
+#   [DIRTY]
+#   [ALWAYS]
+#   [LONG]
+# )
+function(krcmake_get_git_describe)
+    set(options "QUIET;ALL;DIRTY;ALWAYS;LONG")
+    set(one_value_keywords "OUTPUT_VARIABLE;WORKING_DIRECTORY")
+    set(multi_value_keywords)
+
+    cmake_parse_arguments(
+        PARSE_ARGV 0
+        "arg"
+        "${options}"
+        "${one_value_keywords}"
+        "${multi_value_keywords}"
+    )
+
+    if(NOT arg_OUTPUT_VARIABLE)
+        message(FATAL_ERROR "Missing required value: OUTPUT_VARIABLE")
+    endif()
+
+    if(arg_KEYWORDS_MISSING_VALUES)
+        message(FATAL_ERROR "Missing required value: ${arg_KEYWORDS_MISSING_VALUES}")
+    endif()
+
+    if(arg_UNPARSED_ARGUMENTS)
+        message(FATAL_ERROR "Unparsed arguments: ${arg_UNPARSED_ARGUMENTS}")
+    endif()
+
+    if(NOT Git_FOUND)
+        if(NOT arg_QUIET)
+            message(
+                FATAL_ERROR
+                "Could not find \"Git\" package configuration file."
+            )
+        endif()
+        return()
+    endif()
+
+    set(command_options)
+
+    if(arg_ALL)
+        list(APPEND command_options "--all")
+    endif()
+
+    if(arg_DIRTY)
+        list(APPEND command_options "--dirty")
+    endif()
+
+    if(arg_ALWAYS)
+        list(APPEND command_options "--always")
+    endif()
+
+    if(arg_LONG)
+        list(APPEND command_options "--long")
+    endif()
+
+    set(cmd_args
+        COMMAND "${GIT_EXECUTABLE}" describe ${command_options}
+        WORKING_DIRECTORY "${arg_WORKING_DIRECTORY}"
+        RESULT_VARIABLE result
+        OUTPUT_VARIABLE git_describe
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+
+    if(arg_QUIET)
+        list(APPEND cmd_args "ERROR_QUIET")
+    endif()
+
+    execute_process(${cmd_args})
+
+    if(result AND NOT arg_QUIET)
+        message(FATAL_ERROR "Failed to get git description: ${result}")
+    endif()
+
+    set("${arg_OUTPUT_VARIABLE}" "${git_describe}" PARENT_SCOPE)
+endfunction()
