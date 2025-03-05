6a722aaad65131b1a9b96ba7794b71e7e0d20573
JanSimplify <101509908+JanSimplify@users.noreply.github.com>
2025-03-05 21:55:51 +0800

    test: 添加target_compile_options

---
+++ tests/target_compile_options/c_option.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: MIT */
+/* SPDX-FileCopyrightText: Copyright (c) 2025 JanSimplify */
+
+#pragma once
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+    void print_test_define_of_c_file(void);
+
+#ifdef __cplusplus
+}
+#endif
---
+++ tests/target_compile_options/main.cpp
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: MIT */
+/* SPDX-FileCopyrightText: Copyright (c) 2025 JanSimplify */
+
+#include <iostream>
+
+#include "c_option.h"
+#include "cxx_option.hpp"
+
+int main()
+{
+    print_test_define_of_c_file();
+    print_test_define_of_cxx_file();
+    return 0;
+}
